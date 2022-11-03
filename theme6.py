f0a587715104a18ed5d82b85a8d7ee882c450cb2
syadool <takerumorimoto0417@gmail.com>
2022-11-03 20:08:56 +0900

    数字の生成機構の完成。

--- theme6.py
+++
@@ -1,9 +0,0 @@
-int = [9, 0]
-
-while int[0] > int[1]:
-    int.append(int[0] * 10 + int[1])
-    int.append(int[1] * 10 + int[0])
-    int.append(int[2] - int[3])
-    print(int[4])
-    int[0] -= 1
-    int[1] += 1
